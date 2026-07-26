4ca6cb89411deb8905dc2e50d73def2be3a79b01
Stanislav <42679599+Stanislavzzz@users.noreply.github.com>
2026-07-26 17:13:42 +0300

    Add lesson.16

---
+++ m2/lesson.16/linux_web/main.py
@@ -0,0 +1,49 @@
+print('main.py -main 111')
+# комментарий
+print(123)
+print('nano')
+
+print('main.py -main 222')
+# комментарий
+print(222)
+print('nano')
+
+print('main.py -main 333')
+# комментарий
+print(333)
+print('nano')
+
+print('main.py -main 333')
+# комментарий
+print(333)
+print('nano')
+
+print('main.py -main 333')
+# комментарий
+print(333)
+print('nano')
+
+print('main.py -main 444')
+# комментарий
+print(444)
+print('nano')
+
+print('main.py -main 444')
+# комментарий
+print(444)
+print('nano')
+
+print('main.py -main 444')
+# комментарий
+print(444)
+print('nano')
+
+print('main.py -main 444')
+# комментарий
+print(444)
+print('nano')
+
+print('main.py -main 555')
+# комментарий
+print(555)
+print('nano')
